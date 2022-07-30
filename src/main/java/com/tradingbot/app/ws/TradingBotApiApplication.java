da70f755522bee3d61a2234861389c13a2a6d68f
Patrick Daniel Carter <patrick.carter@stellarims.com>
2022-07-29 23:16:08 -0500

    init commit

---
+++ src/main/java/com/tradingbot/app/ws/TradingBotApiApplication.java
@@ -0,0 +1,13 @@
+package com.tradingbot.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingBotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradingbot/app/ws/TradingBotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingbot.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
